285f5697ed0cd13d738e9308fb4bacee2894e7c0
AliSerGok <98125147+AliSerGok@users.noreply.github.com>
2023-10-13 00:16:47 +0300

    Update employee-directory.sql

--- EmployeeManagementSystem/spring-mvc-security-jdbc/spring-mvc-security-jdbc/sql-scripts/employee-directory.sql
+++ EmployeeManagementSystem/spring-mvc-security-jdbc/spring-mvc-security-jdbc/sql-scripts/employee-directory.sql
@@ -20,11 +20,11 @@ CREATE TABLE `employee` (
 --
 
 INSERT INTO `employee` VALUES 
-	(1,'Leslie','Andrews','leslie@luv2code.com'),
-	(2,'Emma','Baumgarten','emma@luv2code.com'),
-	(3,'Avani','Gupta','avani@luv2code.com'),
-	(4,'Yuri','Petrov','yuri@luv2code.com'),
-	(5,'Juan','Vega','juan@luv2code.com');
+	(1,'Leslie','Andrews','leslie@gmail.com'),
+	(2,'Emma','Baumgarten','emma@gmail.com'),
+	(3,'Avani','Gupta','avani@gmail.com'),
+	(4,'Yuri','Petrov','yuri@gmail.com'),
+	(5,'Juan','Vega','juan@gmail.com');
 
 
 DROP TABLE IF EXISTS `roles`;
